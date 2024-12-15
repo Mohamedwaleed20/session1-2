c9f1516836bb90b04e37ba69f624a62127c29c82
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-12-15 22:58:31 +0200

    Add files via upload

---
+++ session1&2.dart
@@ -0,0 +1,147 @@
+void main() {
+  /*var x = 5.4;
+  x = 4.6;
+  const مش هيطبع  لو 
+  print("sum is $x");
+  num num3 = 50;
+  num num4 = 50.4;
+  print("Num 3 is $num3");
+  print("Num 4 is $num4");*/
+  /*double sum = 10.1010101010;
+  print(sum.toStringAsFixed(2));*/
+  /*num number = 2;
+  String withoutRawString = "The value of price is \t $number"; regular result
+  print(withoutRawString);
+  */
+  /*num number = 2;
+  String withoutRawString = r"The value of price is \t $number"; with raw result
+  print(withoutRawString);
+  int one = 1;
+  print("Type of one is ${one.runtimeType}");
+  String s = one.toString();
+  print("Value of oneInString is $s");
+  print("Type of oneInString is ${s.runtimeType}");
+  String strvalue = "1";
+  print("Value of oneInString is $strvalue");
+  int w = int.parse(strvalue);
+  print("Type of oneInString is ${w.runtimeType}");
+  List<String> names = ["Raj", "John", "Max"];
+  print(names);
+  print(names.length);
+  print(names[2]);
+  Set<String> weekday = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  print(weekday);
+  Map<String, String> myDetails = {
+    'name': 'John Doe',
+    'address': 'USA',
+    'fathername': 'Soe Doe'
+  };
+  print(myDetails['address']);
+  String value = "a";
+  print(value.runes);
+  print(value is int);
+  dynamic myVariable = 50;
+  myVariable = "Hello";
+  print(myVariable);
+  print("Enter number:");
+  int? number = int.parse(stdin.readLineSync()!);
+  print("reasult is $number");
+  String firstName = "John";
+  String lastName = "Doe";
+  print("Using +, Full Name is " + firstName + " " + lastName + ".");
+  print("Using interpolation, full name is $firstName $lastName.");
+  String str = "Hi";
+  print(str.codeUnits);
+  print(str.isEmpty);
+  print(str.isNotEmpty);
+  String text = "I am a good boy I like milk. Doctor says milk is good for health.";
+  
+String newText = text.replaceAll("milk", "water"); 
+ 
+print("Original Text: $text");
+print("Replaced Text: $newText");  
+String item1 = "Apple"; 
+   String item2 = "Ant"; 
+   String item3 = "Basket"; 
+   
+   print("Comparing item 1 with item 2: ${item1.compareTo(item2)}"); 
+   print("Comparing item 1 with item 3: ${item1.compareTo(item3)}"); 
+   print("Comparing item 3 with item 2: ${item3.compareTo(item2)}"); 
+   int number = 20;     
+String result = number.toString(); 
+  
+print("Type of number is ${number.runtimeType}");  
+print("Type of result is ${result.runtimeType}");  
+  String allNames = "Ram, Hari, Shyam, Gopal";
+
+  List<String> listNames = allNames.split(",");
+  print("Value of listName is $listNames");
+
+  print("List name at 0 index ${listNames[0]}");
+  print("List name at 1 index ${listNames[1]}");
+  print("List name at 2 index ${listNames[2]}");
+  print("List name at 3 index ${listNames[3]}");
+  String text = "I love computer"; 
+   print("Print only computer: ${text.substring(7)}"); 
+   print("Print only love: ${text.substring(2,6)}");
+  String input = "Hello";
+  print("$input Reverse is ${input.split('').reversed.join()}");
+  String text = "hello world";
+  print(
+      "Capitalized first letter of String: ${text[0].toUpperCase()}${text.substring(1)}");
+  int degree1 = 50;
+  int degree2 = 60;
+  int degree = 55;
+  if (degree > degree2) {
+    print("succeeded");
+  } else if (degree > degree1) {
+    print("semitried");
+  } else {
+    print("failed");
+  }
+          int num1 = 1200;
+        int num2 = 1000;
+        int num3 = 150;
+
+        if(num1 > num2  && num1 > num3){
+            print("Num 1 is greater: i.e $num1");
+        }
+        if(num2 > num1 && num2 > num3){
+           print("Num2 is greater: i.e $num2");
+        }
+        if(num3 > num1 && num3 > num2){
+            print("Num3 is greater: i.e $num3");
+        }
+  var age = 20;
+  assert(age == 20, "you must 20");
+  var dayOfWeek = 5;
+  switch (dayOfWeek) {
+    case 1:
+      print("Day is Sunday.");
+      break;
+    case 2:
+      print("Day is Monday.");
+      break;
+    case 3:
+      print("Day is Tuesday.");
+      break;
+    case 4:
+      print("Day is Wednesday.");
+      break;
+    case 5:
+      print("Day is Thursday.");
+      break;
+    case 6:
+      print("Day is Friday.");
+      break;
+    case 7:
+      print("Day is Saturday.");
+      break;
+    default:
+      print("Invalid Weekday.");
+      break;
+  }
+  var selection = 2;
+  var output = (selection == 2) ? 'Apple' : 'Banana';
+  print(output);*/
+}
